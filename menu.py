23cb6489d77d78b5345b94f69c8f118d5e1fdfba
Ofir <ofir68681@gmail.com>
2021-06-13 16:17:22 +0300

    menu

---
+++ menu.py
@@ -0,0 +1,17 @@
+from ravkav_card import RavKavCard
+
+
+def menu(crd: RavKavCard) -> None:
+    while True:
+        print("Select action: \n 1.Check balance. \n 2.Pay for a ride. \n 3.Deposit.")
+        choice = int(input("Enter your selection: "))
+        if choice == 1:
+            print(crd.check_money())
+        elif choice == 2:
+            amount = int(input("How much you want to pay?"))
+            crd.pay(amount)
+            print("You got now:", crd.check_money())
+        elif choice == 3:
+            amount = int(input("How much you want to deposit?"))
+            crd.deposit(amount)
+            print("You got now:", crd.check_money())
--- ravkav_card.py
+++ ravkav_card.py
@@ -38,17 +38,4 @@ class RavKavCard:
         money = random.randint(0, 1000)
         return RavKavCard(id_num, profile, money)
 
-    def menu(self)-> None:
-        while True:
-            print("Select action: \n 1.Check balance. \n 2.Pay for a ride. \n 3.Deposit.")
-            choice = int(input("Enter your selection: "))
-            if choice == 1:
-                print(self.check_money())
-            elif choice == 2:
-                amount = int(input("How much you want to pay?"))
-                self.pay(amount)
-                print("You got now:", self.check_money())
-            elif choice == 3:
-                amount = int(input("How much you want to deposit?"))
-                self.deposit(amount)
-                print("You got now:", self.check_money())
+
